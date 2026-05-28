78d90da89bf63a925b6d0143b8e9d1261e595ae9
mbmb-mbmb <markus.bonsdorff@iki.fi>
2026-05-28 11:54:08 +0300

    rebased(error_handling): rebased

--- lib/irc_fatstruct.hpp
+++ lib/irc_fatstruct.hpp
@@ -39,7 +39,7 @@ typedef struct	s_IRC_ChannelMembership
 	t_bmask			state;
 	t_IRC_Client*	client;
 }					t_IRC_ChannelMembership;
-static_assert(sizeof(t_IRC_ChannelMembership) <= 1*CACHE_LINE_SIZE,"IRC_ChannelMembership did not use 1 cache line" );
+// static_assert(sizeof(t_IRC_ChannelMembership) <= 1*CACHE_LINE_SIZE,"IRC_ChannelMembership did not use 1 cache line" );
 
 // IRC_Channel state bitmask definitions
 # define	IS_RUNNING BIT(0)
@@ -60,7 +60,7 @@ typedef struct	s_IRC_Channel
 	t_IRC_ChannelMembership*	members;
 	int							member_count;
 }								t_IRC_Channel;
-static_assert(sizeof(t_IRC_Channel) <= 2*CACHE_LINE_SIZE," t_IRC_Channel did not use 2 cache line" );
+// static_assert(sizeof(t_IRC_Channel) <= 2*CACHE_LINE_SIZE," t_IRC_Channel did not use 2 cache line" );
 // IRC_Client state bitmask definitions
 //NOTE: state is essentially an error code catcher for the IRC_Client. BIT(0) means client is in and chatting away. Anything else is an active state that needs to be resolved in some way.
 # define	IS_OK BIT(0)
@@ -77,7 +77,7 @@ typedef struct	s_IRC_Client
 	t_IRC_Channel*		joined_channels;
 	int					joined_count;
 }						t_IRC_Client;
-static_assert(sizeof(t_IRC_Client) <= 4*CACHE_LINE_SIZE," t_IRC_Client did not use 3 cache line" );
+// static_assert(sizeof(t_IRC_Client) <= 4*CACHE_LINE_SIZE," t_IRC_Client did not use 3 cache line" );
 
 // IRC_Server state bitmask definitions
 //NOTE: state is essentially an error code catcher for the IRC_Server. BIT(0) means server is running smoothly, anything else is an error case.
