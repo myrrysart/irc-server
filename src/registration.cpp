9c948a1ec8ff9710b27a052066901806ab402469
Yonatan Kadosh <167137372+conaffetto@users.noreply.github.com>
2026-05-30 13:31:20 +0300

    Parsing(init): Ports notes regarding client registration into new 'registration' branch (#2)

---
+++ src/registration.cpp
@@ -0,0 +1,75 @@
+
+// ======================== CLIENT REGISTRATION ================================
+
+// FIXME: Should we accept multiple connections from the same client (perhaps there
+// is a way for them to connect once, and then somehow change their nickname/name to a
+// valid one, and then they could be still validated again? There is a way to check
+// the IP address via the socket address info struct of the client.... but is it necessary?)
+
+// TODO: IF THE USER IS IDLE VERY LONG TIME, KICK THEM OUT!!!
+// BUT ONLY DO THAT DURING THEIR REGISTRATION PHASE:
+// Do not kick them out for being inactive after they have registered!
+
+// TODO: Consider cases of commands that are received when they are not valid
+// anymore - for example, a nickname has to be set before
+
+// NOTE: "If a command is sent from a client to a server with less parameters
+// than the command requires to be processed, the server will reply with an
+// ERR_NEEDMOREPARAMS (461) numeric and the command will fail."
+
+// NOTE: "Clients MUST NOT include a source when sending a message. Servers MAY
+// include a source on any message, and MAY leave a source off of any message.
+// Clients MUST be able to process any given message the same way whether it
+// contains a source or does not contain one."
+
+// WARN: When a messsage is unknown: Irssi at least returns : "Unknown command: <the_command>"
+// ----> but do not output 'the_command' with a different case, keep case sensitivity.
+// WARN: Only when dispatching the Verb (command) and looking for a match: temporarily
+// make a proper to_upper() for the verb, and only then compare, or something of that nature.
+
+// TODO: NICK: cap nicknames at 30 characters, and trim any characters beyond that
+// without saying anything. Example from the old Horse docs:
+// 'dan-is-my-name-dont-wear-it-out-at-all' became: 'dan-is-my-name-dont-wear-it-ou'
+// NOTE: In this example on Horse, the returned welcome 001 message does not
+// end with the nickname at all, unlike other, shorter examples?!
+
+// TODO: "When sending messages, ensure that a pair of \r\n characters follows
+// every single message your software sends out"
+/*
+ * NOTE: CLIENT AUTHENTIFICATION: unused draft, probably deprecated
+void	parse_input(const std::string &message, t_IRC_Server &IRC_Server,
+			const size_t i) // NOTE: i is the index of the concerned client.
+{
+	// authenticate user - should not be part of a loop, only a check for the
+	// correct bit flag at the top before entering it...
+
+	size_t	 j;
+
+	if (IRC_Server.clients.i.state & ERROR_FLAG)
+		j = 0;
+	else if (IRC_Server.clients.i.state & IS_OK)
+		j = 1;
+	else
+	{
+		if (!(IRC_Server.clients.i.state & PASSWORD))
+
+
+	}
+
+
+
+	if (!(IRC_Server.clients.i.state & IS_OK))
+		authenticate_client();
+
+
+
+}
+
+void	authenticate_client(const std::string &message, t_IRC_Server &IRC_Server,
+			const size_t i)
+{
+	
+
+
+}
+*/
