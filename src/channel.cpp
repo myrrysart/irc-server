f99d7bf1018f892a3aad8803d4aab2b9b65e1585
mbmb-mbmb <markus.bonsdorff@iki.fi>
2026-06-17 10:12:40 +0300

    feat(channel): basic join implemented. No messages back for now.

--- src/channel.cpp
+++ src/channel.cpp
@@ -2,6 +2,7 @@
 #include "../lib/channel.hpp"
 #include "../lib/numerics.hpp"
 
+//TODO: messages back to client(s)
 
 void execute_JOIN_cmd(t_IRC_Client &client, t_IRC_Server &server)
 {
@@ -13,5 +14,11 @@ void execute_JOIN_cmd(t_IRC_Client &client, t_IRC_Server &server)
 	//link client to channel
 	std::string_view	channel_name = client.parser.params[0];
 	t_IRC_Channel 		&channel = server.channels[std::string(channel_name)];
+	//link channel to client
 	client.joined_channels.insert(&channel);
+	channel.members[&client] = 0; //initial flags (no flags)
+	//TODO: might need the operator flag up if first on channel?
 }
+
+// bit manipulations
+// channel.members[&client] = (channel.members.empty()) ? IS_OPERATOR : 0;
