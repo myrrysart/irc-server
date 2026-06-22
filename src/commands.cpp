65fcc40b4c15b13de1a16c9552fd152eda13de8d
Markus Bonsdorff <mbonsdor@c3r1p1.hive.fi>
2026-06-22 14:16:56 +0300

    wip(channel): compiles and runs. Joining a channel seems to work.

--- src/commands.cpp
+++ src/commands.cpp
@@ -69,7 +69,7 @@ void	dispatch_client_command(t_IRC_Client &client, t_IRC_Server &server)
 			case 0:  execute_PASS_cmd(client, server);		break;
 			case 1:  execute_NICK_cmd(client, server);		break;
 			case 2:  execute_USER_cmd(client);				break;
-			case 3:  execute_QUIT_cmd(client, server);		break;
+			// case 3:  execute_QUIT_cmd(client, server);		break;
 			case 4:  execute_JOIN_cmd(client, server);		break;
 			case 5:  execute_PART_cmd(client, server);		break;
 			// case 6:  execute_PRIVMSG_cmd(client, server);	break;
@@ -79,8 +79,8 @@ void	dispatch_client_command(t_IRC_Client &client, t_IRC_Server &server)
 			// case 10: execute_TOPIC_cmd(client, server); 	break;
 			// case 13: execute_NAMES_cmd(client, server); 	break;
 			// case 14: execute_LIST_cmd(client, server);  	break;
-			// case 11: execute_PING_cmd(client, server);		break;
-			// case 12: execute_PONG_cmd(client, server);		break;
+			case 11: execute_PING_cmd(client, server);		break;
+			case 12: execute_PONG_cmd(client, server);		break;
 		}
 	}
 
@@ -88,62 +88,62 @@ void	dispatch_client_command(t_IRC_Client &client, t_IRC_Server &server)
 }
 
 // WARN: work in progress
-void    execute_QUIT_cmd(t_IRC_Client &client, t_IRC_Server &server)
-{
-    if (is_flag_set(client.state, t_IRC_Client::REGISTERED) &&
-        !client.joined_channels.empty())
-    {
-        // start building the message to be sent - do it only once so that it
-        // is easy to be appended to multiple clients.
-        static constexpr std::string_view    middle_part{" QUIT :Quit: "};  // this is a string literal
+// void    execute_QUIT_cmd(t_IRC_Client &client, t_IRC_Server &server)
+// {
+//     if (is_flag_set(client.state, t_IRC_Client::REGISTERED) &&
+//         !client.joined_channels.empty())
+//     {
+//         // start building the message to be sent - do it only once so that it
+//         // is easy to be appended to multiple clients.
+//         static constexpr std::string_view    middle_part{" QUIT :Quit: "};  // this is a string literal
 
-        size_t    len = client.nick.size() + client.username.size() +
-            sizeof(client.hostname) + middle_part.size() +
-            client.parser.params[0].size() + 4; // 4: '!' + '@' + '\r' + '\n'
+//         size_t    len = client.nick.size() + client.username.size() +
+//             sizeof(client.hostname) + middle_part.size() +
+//             client.parser.params[0].size() + 4; // 4: '!' + '@' + '\r' + '\n'
 
-        std::string    quit_msg;
-        quit_msg.reserve(len);
-        append_nick_user_host(quit_msg, client);
-        quit_msg += " QUIT :Quit: ";
-        if (client.parser.n_params)
-            quit_msg += client.parser.params[0];
-        quit_msg += "\r\n";
+//         std::string    quit_msg;
+//         quit_msg.reserve(len);
+//         append_nick_user_host(quit_msg, client);
+//         quit_msg += " QUIT :Quit: ";
+//         if (client.parser.n_params)
+//             quit_msg += client.parser.params[0];
+//         quit_msg += "\r\n";
 
-        // temporary placeholder definition for this struct, if this is kept, it would go elsewhere, of course:
-        // this idea for this struct is to allow keeping track of the clients that have already gotten the message
-        // appended to their output buffer, so that they would not end up getting duplicate messages just because they share
-        // more than one channel with the quitting client.
-        // This may be specific for QUIT command, however.
-        struct    s_already_sent
-        {
-           // MAX_CLIENTS can/should be replaced with whatever maximal value of clients per channel we have.
-           // or maybe there is a better way to achieve that, since the unordered_set of channel members has a size?
-           // but maybe using that would be a VLA (variable length array), which is always a bad idea.
-            int        fds[MAX_CLIENTS];
-              size_t    count;
-        };
+//         // temporary placeholder definition for this struct, if this is kept, it would go elsewhere, of course:
+//         // this idea for this struct is to allow keeping track of the clients that have already gotten the message
+//         // appended to their output buffer, so that they would not end up getting duplicate messages just because they share
+//         // more than one channel with the quitting client.
+//         // This may be specific for QUIT command, however.
+//         struct    s_already_sent
+//         {
+//            // MAX_CLIENTS can/should be replaced with whatever maximal value of clients per channel we have.
+//            // or maybe there is a better way to achieve that, since the unordered_set of channel members has a size?
+//            // but maybe using that would be a VLA (variable length array), which is always a bad idea.
+//             int        fds[MAX_CLIENTS];
+//               size_t    count;
+//         };
 
-           // init that struct, careful: fds contains garbage values, always tread with 'count' size.
-           static    s_already_sent    already_sent;
-           already_sent.count = 0;
+//            // init that struct, careful: fds contains garbage values, always tread with 'count' size.
+//            static    s_already_sent    already_sent;
+//            already_sent.count = 0;
 
-           // iterate through all channels the client is connected to.
-           for (std::unordered_set<t_IRC_Channel>::const_iterator channel_it = client.joined_channels.begin(); channel_it != client.joined_channels.end(); ++channel_it)
-           {
-               // iterate through all members connected to the current channel. Syntax can of course be replaced by 'auto'
-               for (std::unordered_set<t_IRC_ChannelMembership>::const_iterator member_it = channel_it.members.begin(); member_it != channel_it.members.end(); ++member_it;)
-               {
-                   // no need to send the message to itself
-                   // nor to a member that already has the message ready in its buffer
-                   if (member_it.fd == client.fd || has_received_message(already_sent, member_it.fd))
-                       continue;
-                   member_it.send_message_buffer += quit_msg;
-                           // can be replaced with whatever appending function (something like 'build_msg_to_send(member_it.send_message_buffer));'
-                   already_sent.fds[already_sent.count] = member_it.fd;
-                   ++(already_sent.count);
-               }
-           }
-       }
-       // set the disconnect flag (this is for QUIT, of course)
-       client.state |= t_IRC_Client::DISCONNECT;
-   }
+//            // iterate through all channels the client is connected to.
+//            for (std::unordered_set<t_IRC_Channel>::const_iterator channel_it = client.joined_channels.begin(); channel_it != client.joined_channels.end(); ++channel_it)
+//            {
+//                // iterate through all members connected to the current channel. Syntax can of course be replaced by 'auto'
+//                for (std::unordered_set<t_IRC_ChannelMembership>::const_iterator member_it = channel_it.members.begin(); member_it != channel_it.members.end(); ++member_it;)
+//                {
+//                    // no need to send the message to itself
+//                    // nor to a member that already has the message ready in its buffer
+//                    if (member_it.fd == client.fd || has_received_message(already_sent, member_it.fd))
+//                        continue;
+//                    member_it.send_message_buffer += quit_msg;
+//                            // can be replaced with whatever appending function (something like 'build_msg_to_send(member_it.send_message_buffer));'
+//                    already_sent.fds[already_sent.count] = member_it.fd;
+//                    ++(already_sent.count);
+//                }
+//            }
+//        }
+//        // set the disconnect flag (this is for QUIT, of course)
+//        client.state |= t_IRC_Client::DISCONNECT;
+//    }
