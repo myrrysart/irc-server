db025735f0fbeba55b38be2fc89459092aeec499
Yonatan Kadosh <167137372+conaffetto@users.noreply.github.com>
2026-07-09 20:14:01 +0300

    Cleanup: cleans up indentation of one header file's prototypes

--- lib/channel.hpp
+++ lib/channel.hpp
@@ -24,14 +24,14 @@ void	execute_NAMES_cmd(t_IRC_Client &client, t_IRC_Server &server);
 // /list
 void	execute_LIST_cmd(t_IRC_Client &client, t_IRC_Server &server);
 // utils
-t_IRC_Client	*find_chmember_by_nick(t_IRC_Channel &channel, std::string_view nick);
-t_IRC_Channel	*find_channel_by_name(t_IRC_Server &server, std::string_view ch_name);
-t_IRC_Client	*find_client_by_nick(t_IRC_Server &server, std::string_view nick);
-void			remove_client_from_channel(t_IRC_Client &client, t_IRC_Channel &channel, t_IRC_Server &server);
-void			broadcast_to_channel(t_IRC_Channel &channel, const std::string &line, t_IRC_Client &client, bool skip_sender);
+t_IRC_Client		*find_chmember_by_nick(t_IRC_Channel &channel, std::string_view nick);
+t_IRC_Channel		*find_channel_by_name(t_IRC_Server &server, std::string_view ch_name);
+t_IRC_Client		*find_client_by_nick(t_IRC_Server &server, std::string_view nick);
+void				remove_client_from_channel(t_IRC_Client &client, t_IRC_Channel &channel, t_IRC_Server &server);
+void				broadcast_to_channel(t_IRC_Channel &channel, const std::string &line, t_IRC_Client &client, bool skip_sender);
 std::string_view	next_comma_token(std::string_view list, size_t &pos);
-void			broadcast_to_fellow_channelers_once_per_client(t_IRC_Client &sender, const std::string &msg);
-void			broadcast_nick_change(t_IRC_Client &client, const std::string &old_nick);
+void				broadcast_to_fellow_channelers_once_per_client(t_IRC_Client &sender, const std::string &msg);
+void				broadcast_nick_change(t_IRC_Client &client, const std::string &old_nick);
 // message builders
 void	append_JOIN_msg(std::string &buf, const t_IRC_Client &who, std::string_view chan);
 void	append_PART_msg(std::string &buf, const t_IRC_Client &who,
