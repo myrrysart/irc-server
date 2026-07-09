55cf8a0612bd53a360e20b25dafc66dad96754db
ykadosh <167137372+conaffetto@users.noreply.github.com>
2026-07-09 14:48:34 +0300

    Numeric Replies(cleanup): Cleans up the file a little, for aesthetics #10

--- lib/numerics.hpp
+++ lib/numerics.hpp
@@ -19,6 +19,7 @@ void	build_RPL_YOURHOST(t_IRC_Client &client);                        // 002
 void	build_RPL_CREATED(t_IRC_Client &client);                         // 003
 void	build_RPL_MYINFO(t_IRC_Client &client);                          // 004
 void	build_RPL_ISUPPORT(t_IRC_Client &client);                        // 005
+void	build_RPL_UMODEIS(t_IRC_Client &client);                         // 221
 void	build_RPL_LIST(t_IRC_Client &client,                             // 322
 			const t_IRC_Channel &channel);
 void	build_RPL_LISTEND(t_IRC_Client &client);                         // 323
@@ -34,8 +35,10 @@ void	build_RPL_NAMREPLY(t_IRC_Client &client,                         // 353
 			std::string_view channel, std::string_view line);
 void	build_RPL_ENDOFNAMES(t_IRC_Client &client,                       // 366
 			std::string_view channel);
-void	build_RPL_UMODEIS(t_IRC_Client &client);					// 221
-void	build_ERR_USERSDONTMATCH(t_IRC_Client &client);				// 502
+void	build_RPL_MOTD(t_IRC_Client &client,                             // 372
+			std::string_view line);
+void	build_RPL_MOTDSTART(t_IRC_Client &client);                       // 375
+void	build_RPL_ENDOFMOTD(t_IRC_Client &client);                       // 376
 
 
 void	build_ERR_NOSUCHNICK(t_IRC_Client &client,                       // 401
@@ -46,13 +49,9 @@ void	build_ERR_CANNOTSENDTOCHAN(t_IRC_Client &client,                 // 404
             std::string_view channel);
 void	build_ERR_TOOMANYCHANNELS(t_IRC_Client &client,                  // 405
             std::string_view channel);
-void	build_ERR_NOORIGIN(t_IRC_Client &client);                    // 409
-void	build_ERR_NORECIPIENT(t_IRC_Client &client);                 // 411
-void	build_ERR_NOTEXTTOSEND(t_IRC_Client &client);                // 412
-void	build_RPL_MOTDSTART(t_IRC_Client &client);                   // 375
-void	build_RPL_MOTD(t_IRC_Client &client,                         // 372
-			std::string_view line);
-void	build_RPL_ENDOFMOTD(t_IRC_Client &client);                   // 376
+void	build_ERR_NOORIGIN(t_IRC_Client &client);                        // 409
+void	build_ERR_NORECIPIENT(t_IRC_Client &client);                     // 411
+void	build_ERR_NOTEXTTOSEND(t_IRC_Client &client);                    // 412
 void	build_ERR_INPUTTOOLONG(t_IRC_Client &client);                    // 417
 void	build_ERR_UNKNOWNCOMMAND(t_IRC_Client &client);                  // 421
 void	build_ERR_NOMOTD(t_IRC_Client &client);                          // 422
@@ -65,7 +64,7 @@ void	build_ERR_USERNOTINCHANNEL(t_IRC_Client &client,                 // 441
             std::string_view channel, std::string_view nick);
 void	build_ERR_NOTONCHANNEL(t_IRC_Client &client,                     // 442
             std::string_view channel);
-void	build_ERR_USERONCHANNEL(t_IRC_Client &client,                // 443
+void	build_ERR_USERONCHANNEL(t_IRC_Client &client,                    // 443
             std::string_view nick, std::string_view channel);
 void	build_ERR_NOTREGISTERED(t_IRC_Client &client);                   // 451
 void	build_ERR_NEEDMOREPARAMS(t_IRC_Client &client);                  // 461
@@ -83,6 +82,7 @@ void	build_ERR_BADCHANMASK(t_IRC_Client &client,                      // 476
 			std::string_view channel);
 void	build_ERR_CHANOPRIVSNEEDED(t_IRC_Client &client,                 // 482
             std::string_view channel);
+void	build_ERR_USERSDONTMATCH(t_IRC_Client &client);                  // 502
 
 
 void	append_common_reply_prefix(std::string &buffer,
