504e95ce8738a04d80e9aaebec21c529481b4c88
Yonatan Kadosh <167137372+conaffetto@users.noreply.github.com>
2026-07-12 01:13:36 +0300

    cleanup: Removes sockaddr_in struct from client structs, no need to store these

--- lib/irc_fatstruct.hpp
+++ lib/irc_fatstruct.hpp
@@ -154,7 +154,6 @@ typedef struct	s_IRC_Client
 	}();
 
 	t_bmask									state;
-	struct sockaddr_in						addr;  //NOTE: Is this needed here?
 	int										fd;
 	std::chrono::steady_clock::time_point	connection_time;
 	std::string_view						nick;
@@ -168,7 +167,7 @@ typedef struct	s_IRC_Client
 	t_parser								parser;
 	std::unordered_set<t_IRC_Channel*>		joined_channels;
 }	t_IRC_Client;
-static_assert(sizeof(t_IRC_Client) <= 128*CACHE_LINE_SIZE," t_IRC_Client did not use 128 cache line" );
+static_assert(sizeof(t_IRC_Client) <= 68*CACHE_LINE_SIZE," t_IRC_Client did not use 68 cache line" );
 
 // IRC_Server state bitmask definitions
 typedef struct	s_IRC_Server
